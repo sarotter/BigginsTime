6bb9f94ff5b040892a073fc0907f11cdc47eb09e
sarotter <sarotter@umich.edu>
2016-07-31 23:41:29 +0200

    Added a manual for the function

---
+++ R/time_info.R
@@ -0,0 +1,24 @@
+# CREATE PACKAGE DATA -------------------------------------------------------------------------------------------------
+
+# 1. Create a planets-create.R script in the R/ folder.
+# 2. Copy the script below.
+# 3. Source the script.
+# 4. Check that a data/ folder was created and that there is a planets.rda file in it.
+
+time <- read.table(text = "
+                      day         date            time
+                       Sunday     2015-12-25      1200
+                       Monday     2015-12-26      1234
+                       Tuesday    2015-12-27      800
+                       Wednesday  2015-12-28      100
+                       Thursday   2015-12-29      930
+                       Friday     2015-12-30      2300
+                       Saturday   2015-12-31      1440
+                      ")
+#
+#
+#
+
+# This will create the .rda file in the data/ folder.
+#
+devtools::use_data(time, overwrite = TRUE)
