2a52a8e2432145fc21641d47043e462d7be736dc
sarotter <sarotter@umich.edu>
2016-07-31 15:28:48 +0200

    Fixed Description and Documentation for as.Time file

--- R/asTime.R
+++ R/asTime.R
@@ -3,7 +3,14 @@
 #   Build and Reload Package:  'Cmd + Shift + B'
 #   Check Package:             'Cmd + Shift + E'
 #   Test Package:              'Cmd + Shift + T'
-
+#' @param time The time to change class
+#' @param date The date for your time. Default is the system date.
+#' @return The same time variable returned in the POSIXct class.
+#' @examples
+#' as.Time(1234)
+#' @export
+#'
+#'
 as.Time <- function(time, date = Sys.Date()) {
   if(class(time) != numeric) {
       stop("Invalid class for time", call. = FALSE)
@@ -17,6 +24,8 @@ as.Time <- function(time, date = Sys.Date()) {
     if(as.numeric(str_sub(time,4)) < 0 || as.numeric(str_sub(time,4)) > 59) {
       stop("Invalid time", call. = FALSE)
     }
+    as.POSIXct(paste(date, time), format = "%Y-%m-%d %H:%M") -> time
+    return(time)
   }
 }
 
